dc2ff3eafa8da678cc5745b1342d1b6657150692
David <javidoru02@gmail.com>
2026-04-24 14:54:57 +0300

    Add: replace to isPolindrom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function isPolindrom(str) {
     return null;
   }
 
-  const word = str.toLowerCase();
+  const word = str.toLowerCase().replace(/" "/g, "");
   let revers = "";
   for (let i = word.length - 1; i >= 0; i--) {
     revers += word[i];
@@ -16,7 +16,7 @@ function isPolindrom(str) {
   return polindrome;
 }
 
-const polindrom = isPolindrom("QWQ");
+const polindrom = isPolindrom("Q W Q");
 if (polindrom === null) {
   console.log("Помилка: аргумент має бути рядком");
 } else if (polindrom) {
